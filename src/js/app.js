fc035e9a0a6abbd6d0c8a0d4449f073cd200a4a9
加藤北斗 <hokuto15@gmail.com>
2019-02-15 18:59:06 +0900

    jQueryでspaテスト

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,9 @@
-import $ from 'jquery'
-import {validate} from './util/form'
+import $ from "jquery";
 
-class arrivederci {
-	constructor() {
-		this.target = document.querySelector("#app");
-		this.word = "";
-		this.wordArray = [];
-		this.count = "";
-		this.wordLength = this.wordJoin().length;
+location.hash = 'bb';
 
-		this.handleEvent();
-	}
+$(window).on("hashchange", () => {
+	console.log("hashchanged!");
+});
 
-	wordJoin() {
-		for(let i = 0; i < 30; i++) {
-			this.wordArray.push("アリ");
-		}
-		this.word = this.wordArray.join("");
-		return `${this.word}アリーヴェデルチ！（さよならだ）`;
-	}
-
-	displayWord() {
-		this.count = this.target.textContent.length;
-		this.target.innerHTML = this.wordJoin().substr(0, this.count + 1);
-		if(this.count === this.wordLength) clearInterval(this.bucciarati);
-		console.log(this.count, this.wordLength,this.wordJoin());
-	}
-
-	handleEvent() {
-		this.bucciarati = setInterval(() => {
-			this.displayWord();
-		}, 50);
-	}
-}
-
-new arrivederci();
+//初期表示
