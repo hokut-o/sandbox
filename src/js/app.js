4df7f926cc986acf7bf4d09a22b35138b49bf792
加藤北斗 <hokuto15@gmail.com>
2019-03-13 19:16:29 +0900

    オブジェクトから配列を生成する

--- src/js/app.js
+++ src/js/app.js
@@ -1,120 +1,92 @@
 import $ from "jquery";
 
-class ChangeContractStyle {
+class allPrefectures {
 	constructor() {
-		this.prefecturesArray = [
-			"北海道",
-			"青森県",
-			"岩手県",
-			"宮城県",
-			"秋田県",
-			"山形県",
-			"福島県",
-			"茨城県",
-			"栃木県",
-			"群馬県",
-			"埼玉県",
-			"千葉県",
-			"東京都",
-			"神奈川県",
-			"新潟県",
-			"富山県",
-			"石川県",
-			"福井県",
-			"山梨県",
-			"長野県",
-			"岐阜県",
-			"静岡県",
-			"愛知県",
-			"三重県",
-			"滋賀県",
-			"京都府",
-			"大阪府",
-			"兵庫県",
-			"奈良県",
-			"和歌山県",
-			"鳥取県",
-			"島根県",
-			"岡山県",
-			"広島県",
-			"山口県",
-			"徳島県",
-			"香川県",
-			"愛媛県",
-			"高知県",
-			"福岡県",
-			"佐賀県",
-			"長崎県",
-			"熊本県",
-			"大分県",
-			"宮崎県",
-			"鹿児島県",
-			"沖縄県"
+		this.prefectures = [
+			{value: 1, name: "北海道"},
+			{value: 2, name: "青森県"},
+			{value: 3, name: "岩手県"}
 		];
-
-		this.handleEvent();
-		this.displaySelectbox();
-	}
-
-	displayContractstyle() {
-		this.$target
-		.removeClass("is-active")
-		.hide()
-		.eq(this.num - 1)
-		.addClass("is-active")
-		.fadeIn();
-	}
-
-	returnPrefecture() {
 		
+		this.handleEvent();
+		this.test();
 	}
 
-	displaySelectbox() {
-		this.allPrefecturesBody = this.prefecturesArray.map((value, index, array)=>{
-			return value;
+	displayInit() {
+		$.each(this.prefectures, (index, val) => {
+			$("#allPrefecture").append(`
+				<select id="prefecture${index + 1}">
+					<option>都道府県</option>
+				</select>
+				<select id="city${index + 1}">
+					<option>全域</option>
+				</select>
+			`);
 		});
+	}
 
-		console.log(this.allPrefecturesBody);
+	displayPrefecture() {
+		for(let j = 1; j < this.prefectures.length + 1; j++) {
+			$.each(this.prefectures, (index, val) => {
+				if(val.value === j) {
+					$(`#prefecture${j}`).append(`
+						<option value="${val.value}" selected>${val.name}</option>
+					`);
+				} else {
+					$(`#prefecture${j}`).append(`
+						<option value="${val.value}">${val.name}</option>
+					`);
+				}
+			});
+		}
+	}
 
-		$.each(this.prefecturesArray,(index, val) => {
-			this.allPrefecturesBody = `
-				<div class="col-lg-5 col-md-5 col-sm-10 mb-10 pref-clone-block" workplace-index="${index+1}">
-					<div class="input-group clearfix">
-						<label for="" class="pref-label input-group-addon font-w600 col-md-4 col-3 font-size-xsmall-sp">都道府県</label>
-						<select class="pref-selectbox form-control col-md-8 add-js-select2 select2-hidden-accessible" replace-name="" style="width:100%" name="workplace[${index+1}][prefecture_id]" tabindex="-1" aria-hidden="true">
-							<option value="">都道府県</option>
-						</select>
-					</div>
-				</div>
-
-				<div class="col-lg-5 col-md-5 col-sm-10 mb-10 city-clone-block" workplace-index="${index+1}">
-					<div class="input-group clearfix">
-						<label for="" class="city-label input-group-addon font-w600 col-md-4 col-3 font-size-xsmall-sp">市区町村</label>
-						<select class="city-selectbox form-control col-md-8 add-js-select2 select2-hidden-accessible" replace-name="" style="width:100%" name="workplace[${index+1}][city_id]" tabindex="-1" aria-hidden="true">
-							<option value="">全域</option
-						</select>>
-
-						<span class="select2 select2-container select2-container--default" dir="ltr" style="width: 100%;">
-							<span class="selection">
-								<span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-workplace${index+1}city_id-77-container">
-									<span class="select2-selection__rendered" id="select2-workplace2city_id-77-container" title="全域">全域</span>
-									<span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span>
-								</span>
-							</span>
-							<span class="dropdown-wrapper" aria-hidden="true"></span>
-						</span>
-					</div>
-				</div>
-			`;
-		});
+	displayCity() {
+		for(let k = 1; k < this.prefectures.length + 1; k++) {
+			$.ajax({
+				url: `https://stg-worker.urumap.com/api/get_city_with_area?prefecture_id=${k}`,
+				type: "GET",
+				dataType: "json"
+			})
+			.done(res => {
+				$.each(res.data, (key, val) => {
+					for(let i = 0; i < res.data[key].cities.length; i++) {
+						$(`#city${k}`).append(`
+							<option value="${res.data[key].cities[i].id}">${res.data[key].cities[i].name
+							}</option>
+						`);
+					}
+				});
+			})
+			.fail(() => {
+				console.log("error");
+			})
+			.always((jqXHR,textStatus) => {
+				console.log(jqXHR, textStatus);
+			});
+		}
+	}
+	
+	displayReset(){
+		$('#allPrefecture').html('');
+		this.ajax_obj.abort();
+	}
+	
+	test(){
+		this.prefectures[0]["city"] = "test";
 	}
 
 	handleEvent() {
-		$(this.$radioTrigger).on("click", e => {
-			this.num = $(e.currentTarget).data("triggerContractstyle");
-			this.displayContractstyle();
+		$('#display').on('click', () => {
+			this.displayInit();
+			this.displayPrefecture();
+			this.displayCity();
+		});
+		
+		$('#reset').on('click',()=>{
+			this.displayReset();
 		});
 	}
-};
+}
 
-new ChangeContractStyle();
+new allPrefectures();
