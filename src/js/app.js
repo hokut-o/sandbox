82b37272a5fe7a32235fa7eaf8f9be33a6bb2dc8
加藤北斗 <hokuto15@gmail.com>
2019-04-11 18:53:51 +0900

    スクロールしたら要素が出てくるやつ

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 import $ from "jquery";
 
-console.log('hello,world.');
+$(window).on("scroll", () => {
+	let current = $(window).scrollTop();
+	console.log(current);
+	if(current > 500) {
+		$(".footer").addClass("is-active");
+	} else {
+		$(".footer").removeClass("is-active");
+	}
+});
