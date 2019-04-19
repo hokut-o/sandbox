3388fe7466e479d9fbdd16bc4da757e461b223d0
加藤北斗 <hokuto15@gmail.com>
2019-04-19 19:29:29 +0900

    ハッシュ化

---
+++ src/php/hash.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="foo">
+	<meta name="keywords" content="foo">
+	<title>foo</title>
+	<link rel="stylesheet" href="./css/app.css?1554947136419">
+</head>
+<body>
+	<?php
+		$number = hash('md5', 09012341234);
+		$number2 = hash('md5', 201904194739);
+	?>
+	<p><?php echo $number; ?></p>
+	<p><?php echo $number2; ?></p>
+
+	<div class="footer"></div>
+<script src="./js/app.js?1554947136419"></script>
+</body>
+</html>
