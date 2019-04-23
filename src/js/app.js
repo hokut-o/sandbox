63ea1801f66d6f1b9efd44c03f7c3bf3b2b6be79
加藤北斗 <hokuto15@gmail.com>
2019-04-23 19:44:05 +0900

    filterメソッドで特定のkeyの値のみ書き換わった配列を新たに返す

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
-import $ from "jquery";
+const array = [
+  { id: 1, name: "太郎", age: 22 },
+  { id: 2, name: "二郎", age: 22 },
+  { id: 3, name: "三郎", age: 22 },
+  { id: 4, name: "四郎", age: 22 },
+  { id: 5, name: "五郎", age: 22 },
+  { id: 6, name: "六郎", age: 22 }
+];
 
-console.log('hello,world.');
+const filteredArray = array.filter((val) => {
+  return val.age = 23;
+});
+
+console.log(filteredArray);
