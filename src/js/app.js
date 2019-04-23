8c0d67a0716dfd1226c7649bb3ed60c0daa0428a
加藤北斗 <hokuto15@gmail.com>
2019-04-23 20:22:49 +0900

    map

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,16 @@
+import $ from "jquery"
+
 const array = [
-  { id: 1, name: "太郎", age: 22 },
-  { id: 2, name: "二郎", age: 22 },
-  { id: 3, name: "三郎", age: 22 },
-  { id: 4, name: "四郎", age: 22 },
-  { id: 5, name: "五郎", age: 22 },
-  { id: 6, name: "六郎", age: 22 }
+	{id: 1, name: "太郎", age: 22},
+	{id: 2, name: "二郎", age: 22},
+	{id: 3, name: "三郎", age: 22},
+	{id: 4, name: "四郎", age: 22},
+	{id: 5, name: "五郎", age: 22},
+	{id: 6, name: "六郎", age: 22}
 ];
 
-const filteredArray = array.filter((val) => {
-  return val.age = 23;
-});
+const changedArray = array.map(({id, name}) => ({
+	id, name, age: 23
+}));
 
-console.log(filteredArray);
+console.log(changedArray);
