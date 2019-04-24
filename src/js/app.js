8a1ff7fac22fc9916fdfd6899ba834e4a78d89af
加藤北斗 <hokuto15@gmail.com>
2019-04-24 16:01:29 +0900

    bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 import $ from "jquery";
 
-console.log('hello,world.');
+// BootstrapのJavaScript側の機能を読み込む
+import "bootstrap";
+
+console.log("test");
+
+//アニメーション完了じにトリガーするカスタムイベント
+$("#collapseExample").on("shown.bs.collapse", () => {
+	console.log("shown");
+});
