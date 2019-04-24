7c18e7007db696b838605f172225f1a2246cb1bd
加藤北斗 <hokuto15@gmail.com>
2019-04-24 12:55:54 +0900

    mapパターン追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,41 @@
-import $ from "jquery"
+import $ from "jquery";
 
 const array = [
-	{id: 1, name: "太郎", age: 22},
-	{id: 2, name: "二郎", age: 22},
-	{id: 3, name: "三郎", age: 22},
-	{id: 4, name: "四郎", age: 22},
-	{id: 5, name: "五郎", age: 22},
-	{id: 6, name: "六郎", age: 22}
+  { id: 1, name: "太郎", age: 22 },
+  { id: 2, name: "二郎", age: 22 },
+  { id: 3, name: "三郎", age: 22 },
+  { id: 4, name: "四郎", age: 22 },
+  { id: 5, name: "五郎", age: 22 },
+  { id: 6, name: "六郎", age: 22 }
 ];
 
-//map
+//map（非破壊的）
 //-------------------------------------------------
-// const changedArray = array.map(({id, name}) => ({
-// 	id, name, age: 23
-// }));
-//
-// console.log(changedArray);
+const changedArray = array.map(({ id, name }) => ({
+  id: id, //idに省略可
+  name: name, //nameに省略可
+  age: 23
+}));
+
+console.log(changedArray);
+console.log(array);
+
+const animals = [
+  { id: 1, category: "animal", kind: "dog" },
+  { id: 2, category: "animal", kind: "cat" },
+  { id: 3, category: "animal", kind: "bird" }
+];
+
+const kinds = animals.map(animal => animal.kind);
+
+console.log(kinds);
+console.log(animals);
 
 
-//foreach
+//foreach（破壊的）
 //-------------------------------------------------
 // array.forEach((val,index) => {
 // 	return val.age = 23;
 // });
 //
 // console.log(array);
-
