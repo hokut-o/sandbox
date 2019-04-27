6fff6673bcd66880a646dfc5657731edf7259638
加藤北斗 <hokuto15@gmail.com>
2019-04-27 14:47:32 +0900

    vanillaでmenu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
-import $ from "jquery";
-
-console.log('hello,world.');
+var trigger = document.querySelectorAll(".trigger");
+for(var i = 0; i < trigger.length; i++) {
+	trigger[i].addEventListener("mouseenter", e => {
+		var target = e.currentTarget.querySelectorAll(".target");
+		if(target[0].classList.contains("hidden") === true) {
+			return target[0].classList.remove("hidden");
+		}
+	});
+	trigger[i].addEventListener("mouseleave", e => {
+		var target = e.currentTarget.querySelectorAll(".target");
+		target[0].classList.add("hidden");
+	});
+}
