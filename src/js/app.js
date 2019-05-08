a8acd6fd70eca8448de99028e49a3dbb603ffefa
加藤北斗 <hokuto15@gmail.com>
2019-05-08 15:54:26 +0900

    アリアリをキメる

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,20 @@
-import $ from "jquery";
+class arrivederci {
+	constructor() {
+		this.target = document.querySelector("#app");
+		this.word = "アリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリアリーヴェデルチ！（さよならだ）";
+		this.count = "";
+		this.wordLength = this.word.length;
+		this.timer = "";
+		this.displayWord();
+	}
+	
+	displayWord() {
+		const timer = setInterval(() => {
+			this.count = this.target.textContent.length;
+			this.target.innerHTML = this.word.substr(0, this.count + 1);
+			if(this.count === this.wordLength) clearInterval(timer);
+		}, 100);
+	}
+}
 
-console.log('hello,world.');
+new arrivederci();
