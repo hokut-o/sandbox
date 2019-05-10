a3eb6e0413ac5b482eb98bbca535c3200828dced
加藤北斗 <hokuto15@gmail.com>
2019-05-10 16:23:12 +0900

    絞り込み検索

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,42 @@
 import $ from "jquery";
 
-console.log('hello,world.');
+$(function() {
+	$(document).on("click", "button", function() {
+		filter_list();
+	});
+
+	// リセットボタンをクリックしたときの処理
+	$("#reset").on("click", function() {
+		// フォームのselectとcheckboxをリセット
+		$(".serchBox select").val("");
+		// リストの絞り込みをリセット
+		filter_list();
+	});
+	$("#search").on("click", function() {
+		filter_list();
+	});
+
+	// リストを絞り込む関数
+	function filter_list() {
+		var lists = $(".list li");
+		lists.show();
+		for(var i = 0; i < $(".serchBox select").length; i++) {
+			// 絞り込みの項目を取得
+			var item = $(".serchBox select").eq(i).attr("name");
+
+			// 絞り込みの対象を取得
+			var target = $(".serchBox select").eq(i).val();
+
+			console.log(item,target,lists);
+
+			if(target !== "") {
+				for(var j = 0; j < lists.length; j++) {
+					// 絞り込み対象でない場合は非表示
+					if(target !== lists.eq(j).find("." + item).find("span").data("tiiki")) {
+						lists.eq(j).hide();
+					}
+				}
+			}
+		}
+	}
+});
