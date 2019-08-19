c419423fe177589fda2944e03ff5d3aeb9b64b19
加藤北斗 <hokuto15@gmail.com>
2019-08-19 17:54:37 +0900

    amazon pay モック

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
-import $ from "jquery";
+window.onAmazonLoginReady = function () {
+	amazon.Login.setClientId('amzn1.application-oa2-client.affb56afcba4432e9442e52aed2d7dcd');
+};
+window.onAmazonPaymentsReady = function () {
+	showButton();
+};
 
-console.log('hello,world.');
+function showButton() {
+	var authRequest;
+	OffAmazonPayments.Button("AmazonPayButton", "amzn1.application-oa2-client.affb56afcba4432e9442e52aed2d7dcd", {
+		type: "PwA",
+		color: "Gold",
+		size: "large",
+		language: "jp",
+		authorization: function () {
+			loginOptions = {scope: "profile payments:widget", popup: true};
+			authRequest = amazon.Login.authorize(loginOptions, "--- REDIRECT_URL ---");
+		},
+		onError: function (error) {
+			// error handling
+		}
+	});
+};
